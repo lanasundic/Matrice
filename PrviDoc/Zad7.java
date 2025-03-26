f48371e951b240fd124da4dc4032adc380d67574
Lana Sundic <lanasundic@gmail.com>
2025-03-26 21:02:29 +0100

    Adding 2 exercises

---
+++ PrviDoc/Zad7.java
@@ -0,0 +1,32 @@
+//Napisati funkciju int[][] popuniMatricu1(int m, int n, int k) koja za date m i n kreira matricu dimenzija mxn
+// i popunjava je slučajnim brojevima iz intervala [-k,k].
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zad7 {
+
+    public static int[][] popuniMatricu1(int m, int n, int k) {
+        Random rand = new Random();
+        int matr[][] = new int[m][n];
+
+        for (int i = 0; i < matr.length; i++) {
+            for (int j = 0; j < matr.length; j++) {
+                matr[i][j] = 
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Unesite dimenzije matrice:");
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        System.out.println("Unesite k:");
+        int k = scanner.nextInt();
+
+    }
+
+}
---
+++ PrviDoc/Zad8.java
@@ -0,0 +1,10 @@
+/*Napisati funkciju int[][] popuniMatricu2(int n) koja za dato n kreira i popunjava cijelim brojevima kvadratnu matricu reda n na zadati način. Na primjer, za n=4
+        1 2 3 4
+        2 3 4 1
+        3 4 1 2
+        4 1 2 3 
+ */
+
+public class Zad8 {
+    
+}
